30d425b1c32f3682f3674f70a9eae27607ff50b0
Alexey A. Sidnev <alexey.sidnev@gmail.com>
2016-02-27 22:57:12 +0300

    Test

---
+++ solutions/base-version/src/main.cpp
@@ -0,0 +1,19 @@
+#include "Complex.h"
+#include "tests.h"
+#include <stdlib.h>
+#include <iostream>
+
+int main() {
+  srand(666);
+
+  // CheckSort<short int>(10);
+  // CheckSort<int>(10);
+  // CheckSort<char>(10);
+  // CheckSort<float>(10);
+  // CheckSort<double>(10);
+  // CheckSort<Complex<float>>(10);
+  // CheckSort<Complex<int>>(10);
+  // CheckSort<Complex<Complex<float>>>(10);
+
+  return 0;
+}
