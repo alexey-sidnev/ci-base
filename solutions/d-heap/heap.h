3a359d6812cea5ec33459e0698fed324a2c5ecf5
mayya-rozhnova <wooownn@gmail.com>
2016-03-04 00:42:59 +0300

    heap1

---
+++ solutions/d-heap/heap.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Dheap {
+public:
+	int d;
+	int size;
+	int *keys;
+	Dheap(int _s, int _d);
+	~Dheap();
+	void Swap(int i, int j);
+	void Up(int i);
+	void Down(int i);
+	int MinChild(int i);
+	void DoHeap();
+	void Add(int n);
+	void Psort();
+};
